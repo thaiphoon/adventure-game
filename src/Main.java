39352d894d449f49af081f40c87b60e8dec9debb
Niklas Holmström <thaiphoon@users.noreply.github.com>
2025-09-15 11:44:01 +0200

    Defined general structure of the game

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,37 @@
 public class Main {
-    
+    /**
+     * One loop untill dead or quiting
+     * 3+ if-else scenarios
+     * 2+ variables (e.g. gold, stamina)
+     * 2+ methods/functions
+     * Main Class
+     * - One main loop
+     *
+     * OBJECTS
+     * - Player
+     *      Variables
+     *          gold
+     *          stamina
+     *      Methods
+     *          exploreArea
+     *          rest
+     *          getStamina
+     *          updateStamina
+     *
+     * - Enemy
+     *      Variables
+     *          gold
+     *          stamina
+     *      Methods
+     *          getStamina
+     *          updateStamina
+     * - Treasure
+     *      gold
+     *
+     * - Weapon
+     *
+     * - Fight
+     *      Methods
+     *          fight
+     */
 }
