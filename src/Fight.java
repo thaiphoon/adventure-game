734e51c184b8d8ab6fb11881b54dc18faf34cd0d
y_tatsumi@hotmail.com <57840245+ytatumi@users.noreply.github.com>
2025-09-16 13:17:06 +0200

    add constructor and modifies the name of a method for fight

--- src/Fight.java
+++ src/Fight.java
@@ -7,22 +7,29 @@ public class Fight{
     */
 
     private boolean result = false;  // if player win,result = true.
-    private Player player;
-    private Enemy enemy;
-    private int stamina = 1;
+    // private int fightStamina = 1;
+    private final int fightStamina;
+    private final Player player;
+    private final Enemy enemy;
 
-    public boolean fight(Player player, Enemy enemy){
+    public Fight(Player player, Enemy enemy, int fightStamina){
+        this.player = player;
+        this.enemy = enemy;
+        this.fightStamina = fightStamina;
+    }
+
+    public boolean fightEnemy(Player player, Enemy enemy){
         System.out.println("Fight!");
         Random random = new Random();
         result = random.nextBoolean();
         if (result){
             System.out.println("Player won the game.");
-            player.updateStamina(this.stamina);
-            enemy.updateStamina(-this.stamina);
+            player.updateStamina(this.fightStamina);
+            enemy.updateStamina(-this.fightStamina);
         }else{
             System.out.println("Player lost the game.");
-            player.updateStamina(-this.stamina);
-            enemy.updateStamina(this.stamina);
+            player.updateStamina(-this.fightStamina);
+            enemy.updateStamina(this.fightStamina);
         }
         return result;
     }
